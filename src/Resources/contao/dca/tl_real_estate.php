ed488e5ea0b30af87ac01b5e19226261602b7d15
eki89 <fabian@oveleon.de>
2020-05-15 11:08:46 +0200

    Cleanup and exclude flag added in module field.

--- src/Resources/contao/dca/tl_real_estate.php
+++ src/Resources/contao/dca/tl_real_estate.php
@@ -7,6 +7,7 @@
  * @copyright Copyright (c) 2019  Oveleon GbR (https://www.oveleon.de)
  * @license   https://www.contao-estatemanager.com/lizenzbedingungen.html
  */
+
 if(ContaoEstateManager\Featured\AddonManager::valid()) {
     $GLOBALS['TL_DCA']['tl_real_estate']['list']['label']['post_label_callbacks'][] = array('tl_real_estate_featured', 'addFeaturedInformation');
 
@@ -37,12 +38,13 @@ if(ContaoEstateManager\Featured\AddonManager::valid()) {
         ->applyToPalette('default', 'tl_real_estate')
     ;
 }
+
 /**
  * Provide miscellaneous methods that are used by the data configuration array.
  *
  * @author Daniele Sciannimanica <daniele@oveleon.de>
  */
-class tl_real_estate_featured extends Backend
+class tl_real_estate_featured extends Contao\Backend
 {
     /**
      * Import the back end user object
@@ -50,15 +52,7 @@ class tl_real_estate_featured extends Backend
     public function __construct()
     {
         parent::__construct();
-        $this->import('BackendUser', 'User');
-    }
-
-    /**
-     * Check permissions to edit table tl_real_estate
-     */
-    public function checkPermission()
-    {
-        return;
+        $this->import('Contao\BackendUser', 'User');
     }
 
     /**
@@ -73,9 +67,9 @@ class tl_real_estate_featured extends Backend
      *
      * @return string
      */
-    public function iconFeaturedObject($row, $href, $label, $title, $icon, $attributes)
+    public function iconFeaturedObject(array $row, string $href, string $label, string $title, string $icon, string $attributes): string
     {
-        if (\strlen(Contao\Input::get('toid')))
+        if (strlen(Contao\Input::get('toid')))
         {
             $this->toggleFeaturedObject(Contao\Input::get('toid'), (Contao\Input::get('state') == 1), (@func_get_arg(12) ?: null));
             $this->redirect($this->getReferer());
@@ -106,12 +100,11 @@ class tl_real_estate_featured extends Backend
      *
      * @throws Contao\CoreBundle\Exception\AccessDeniedException
      */
-    public function toggleFeaturedObject($intId, $blnVisible, Contao\DataContainer $dc=null)
+    public function toggleFeaturedObject(int $intId, bool $blnVisible, Contao\DataContainer $dc=null): void
     {
         // Check permissions to edit
         Contao\Input::setGet('id', $intId);
         Contao\Input::setGet('act', 'featuredObject');
-        $this->checkPermission();
 
         // Check permissions to feature
         if (!$this->User->hasAccess('tl_real_estate::featuredObject', 'alexf'))
@@ -123,16 +116,16 @@ class tl_real_estate_featured extends Backend
         $objVersions->initialize();
 
         // Trigger the save_callback
-        if (\is_array($GLOBALS['TL_DCA']['tl_real_estate']['fields']['featuredObject']['save_callback']))
+        if (is_array($GLOBALS['TL_DCA']['tl_real_estate']['fields']['featuredObject']['save_callback']))
         {
             foreach ($GLOBALS['TL_DCA']['tl_real_estate']['fields']['featuredObject']['save_callback'] as $callback)
             {
-                if (\is_array($callback))
+                if (is_array($callback))
                 {
                     $this->import($callback[0]);
                     $blnVisible = $this->{$callback[0]}->{$callback[1]}($blnVisible, $dc);
                 }
-                elseif (\is_callable($callback))
+                elseif (is_callable($callback))
                 {
                     $blnVisible = $callback($blnVisible, $this);
                 }
@@ -156,7 +149,7 @@ class tl_real_estate_featured extends Backend
      *
      * @return array
      */
-    public function addFeaturedInformation($row, $label, Contao\DataContainer $dc, $args)
+    public function addFeaturedInformation(array $row, string $label, Contao\DataContainer $dc, array $args): array
     {
         if (!$row['featuredObject'])
         {
